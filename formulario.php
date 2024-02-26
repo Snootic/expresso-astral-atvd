060e62f91906a7d5ace31e7bdceb5066fc9e8211
Snootic <snooticonoob@hotmail.com>
2024-02-26 19:57:32 -0300

    feat: adding more customization

--- formulario.php
+++ formulario.php
@@ -1,5 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+<style>
+    body{
+            background-image: url("imagens/herta-station.jpg");
+            color: white;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+        }
+    .container{
+            background-color: rgba(100, 100, 100, 0.4);
+            padding: 20px;
+            margin:50px;
+            outline: solid 1px white;
+            border-radius: 5%;
+            text-align: center;
+        }
+        footer {
+            background-color: rgba(100, 100, 100, 0.4);
+            outline: solid 1px white;
+            text-align: center;
+            width: 100%;
+            margin-top: 70px;
+        }
+        footer a{
+            color: white; 
+            text-decoration: none;
+        }
+</style>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,41 +37,41 @@
     <link rel="shortcut icon" href="imagens/pom-pom.ico" />
 </head>
 <body>
-<h1>Reserve uma viagem pelo Expresso Astral</h1>
-    <h3>Preencha os dados abaixo</h3>
-    <img src="https://media1.tenor.com/images/bead0ffe77a3fd513511c05dafb492e6/tenor.gif?itemid=18105609" alt="" width="200" height="200">
-    <form>
-        <label for="planeta-origem">Planeta de origem:</label>
-        <input type="text" name="origem" id="planeta-origem"><p>
-        <label for="planeta-destino">Planeta de destino:</label>
-        <input type="text" name="desino" id="planeta-destino"><p>
-        <label for="data-partida">Data de partida:</label>
-        <input type="date" name="partida" id="data-partida"><p>
-        <label for="quantidade-passagens">Quantidade de passagens:</label>
-        <input type="number" name="passagens" id="quantidade-passagens"><p>
-        <input type="submit" value="Reservar" onclick="popup_redirect()">
-    </form>
-    <script>
-    function popup_redirect() {
-        event.preventDefault();
+    <div class='container'>
+        <h1>Reserve uma viagem pelo Expresso Astral</h1>
+            <h3>Preencha os dados abaixo</h3>
+            <img src="https://static.wikia.nocookie.net/houkai-star-rail/images/a/a7/Item_Star_Rail_Special_Pass.png" alt="" width="200" height="200">
+            <form>
+                <label for="planeta-origem">Planeta de origem:</label>
+                <input type="text" name="origem" id="planeta-origem"><p>
+                <label for="planeta-destino">Planeta de destino:</label>
+                <input type="text" name="desino" id="planeta-destino"><p>
+                <label for="data-partida">Data de partida:</label>
+                <input type="date" name="partida" id="data-partida"><p>
+                <label for="quantidade-passagens">Quantidade de passagens:</label>
+                <input type="number" name="passagens" id="quantidade-passagens"><p>
+                <input type="submit" value="Reservar" onclick="popup_redirect()">
+            </form>
+            <script>
+            function popup_redirect() {
+                event.preventDefault();
 
-        var origem = document.getElementById('planeta-origem').value;
-        var destino = document.getElementById('planeta-destino').value;
-        var data = document.getElementById('data-partida').value;
-        var passagem = document.getElementById('quantidade-passagens').value;
-        console.log(origem + destino + data + passagem)
+                var origem = document.getElementById('planeta-origem').value;
+                var destino = document.getElementById('planeta-destino').value;
+                var data = document.getElementById('data-partida').value;
+                var passagem = document.getElementById('quantidade-passagens').value;
+                console.log(origem + destino + data + passagem)
 
-        if (origem !== "" && destino !== "" && data !== "" && passagem !== "") {
-            alert("Reserva feita com sucesso!");
-            // window.location.href = "quem-somos.php";
-        } else {
-            alert("Por favor, preencha todos os campos.");
-        }
-    }
-    </script>
+                if (origem !== "" && destino !== "" && data !== "" && passagem !== "") {
+                    alert("Reserva feita com sucesso!");
+                } else {
+                    alert("Por favor, preencha todos os campos.");
+                }
+            }
+            </script>
+    </div>
 </body>
 <footer>
-    <p>Voltar a página de cadastro</p>
-    <p>Ir para página de reservas</p>
+    <p><a href="index.php">Voltar a página de cadastro</a>&emsp;&emsp;<a href="quem-somos.php">Quem somos?</a></p>
 </footer>
 </html>
--- index.php
+++ index.php
@@ -18,6 +18,14 @@
         padding: auto ;
         text-align: center;
     }
+    .container{
+        background-color: rgba(100, 100, 100, 0.4);
+        padding: 20px;
+        margin-top:50px;
+        outline: solid 1px white;
+        border-radius: 5%;
+        text-align: center;
+    }
 </style>
 <head>
     <meta charset="UTF-8">
@@ -28,22 +36,24 @@
 </head>
 <body>
     <video autoplay muted loop id="video-background">
-        <source src="https://youtube.googleapis.com/embed/?status=ok&hl=pt_BR&allow_embed=0&ps=docs&partnerid=30&autoplay=0&abd=0&docid=1IJAu8x89w0j33xayWWXMlDmZMRQ8MYXH&el=embed&title=HSR%20Wallpaper%20v3.mp4&BASE_URL=https%3A%2F%2Fdrive.google.com%2Fu%2F0%2F&iurl=https%3A%2F%2Flh3.googleusercontent.com%2Fdrive-storage%2FAJQWtBMVGWnPHpeFz-5-K0pfcnAhear90ZQ0yRoURIO4RCKuirzW_9Px0hVL1x5VbscU_J4sZiSL3ijUo-PsIBPsoyx7L-PVVxT4xZSG3sRx%3Ds512&cc3_module=1&reportabuseurl=https%3A%2F%2Fdrive.google.com%2Fu%2F0%2Fabuse%3Fauthuser%3D0%26id%3D1IJAu8x89w0j33xayWWXMlDmZMRQ8MYXH&token=1&plid=V0QXt4fhy3WHKA&timestamp=1708983986160&length_seconds=149&BASE_YT_URL=https%3A%2F%2Fdrive.google.com%2Fu%2F0%2F&cc_load_policy=1&authuser=0&wmode=window&override_hl=1&enablecastapi=0&pipable=1&enablepostapi=1&postid=drive-viewer-video-player-object-0&origin=https%3A%2F%2Fdrive.google.com" type="video/mp4">
+        <source src="imagens/astral-express.mp4" type="video/mp4">
     </video>
-    <h1>Expresso Astral</h1>
-    <p>Faça um cadastro abaixo</p>
-    <img src="https://64.media.tumblr.com/68308cc6e0f5b3faf8d1beb8b81d8675/7cc140a4ab9024c1-9f/s400x600/45808ead03e68d202f153ba4cb52672c4eed02fa.gif" alt="" width="300" height="300">
-    <form>
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" id="nome"><p>
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email"><p>
-        <label for="senha">Senha:</label>
-        <input type="password" name="senha" id="senha"><p>
-        <label for="confirmar_senha">Confirmar Senha:</label>
-        <input type="password" name="confirmar_senha" id="confirmar_senha"><p>
-        <input type="submit" value="Cadastrar" onclick="popup_redirect()">
-    </form>
+    <div class='container'>
+        <h1>Expresso Astral</h1>
+        <p>Faça um cadastro abaixo</p>
+        <img src="https://64.media.tumblr.com/68308cc6e0f5b3faf8d1beb8b81d8675/7cc140a4ab9024c1-9f/s400x600/45808ead03e68d202f153ba4cb52672c4eed02fa.gif" alt="" width="300" height="300">
+        <form>
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome"><p>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email"><p>
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" id="senha"><p>
+            <label for="confirmar_senha">Confirmar Senha:</label>
+            <input type="password" name="confirmar_senha" id="confirmar_senha"><p>
+            <input type="submit" value="Cadastrar" onclick="popup_redirect()">
+        </form>
+    </div>
     <script>
     function popup_redirect() {
         event.preventDefault();
